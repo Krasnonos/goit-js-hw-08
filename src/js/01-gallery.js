51c90ee4e7b33b87a9e5455e4fd4fb6925edb702
Krasnonos <andreykrasnonos@icloud.com>
2022-05-10 18:09:21 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function creareGallaryMarkup(imagesArrey) {
+  return imagesArrey
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="large-image.jpg"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+galleryEl.innerHTML = creareGallaryMarkup(galleryItems);
+
+galleryEl.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    animationSpeed: 100,
+  });
+
+  document.addEventListener('keydown', onCloseModal);
+}
+
+function onCloseModal(evt) {
+  evt.preventDefault();
+
+  if (evt.code === 'Escape') {
+    modalImg.close();
+  }
+
+  document.removeEventListener('keydown', onCloseModal);
+}
