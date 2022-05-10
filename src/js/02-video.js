8b0b9bf597581fba4fb1cc4b7a039b5de5f6c106
Krasnonos <andreykrasnonos@icloud.com>
2022-05-10 18:33:21 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,12 @@
 var throttle = require('lodash.throttle');
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const b = 3;
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
 
-player.on(
-  'timeupdate',
-  throttle(evt => localStorage.setItem('videoplayer-current-time', evt.seconds), 1000),
-);
+// player.on(
+//   'timeupdate',
+//   throttle(evt => localStorage.setItem('videoplayer-current-time', evt.seconds), 1000),
+// );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
